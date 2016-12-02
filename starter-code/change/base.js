69194a57faa63c85ab3944ebb15b2485ce52cd16
Ryan_Thomas <rthoma11@villanova.edu>
2016-12-02 10:02:44 -0800

    edits to change.js

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -3,5 +3,12 @@ console.log("Sanity Check: JS is working!");
 $(document).ready(function(){
 
   $("#time").text( Date.now() );
+  $(".add").change(function() {
+    var left = $("#left").val();
+    var right = $("#right").val();
 
+    total = Number(left) + Number(right);
+
+    $("#total").val(total);
+  })
 })
